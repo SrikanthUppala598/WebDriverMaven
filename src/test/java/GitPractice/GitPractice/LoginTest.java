0c7531511a575ed3d41cb5faaadd6ad7252022eb
SrikanthUppala98 <srikanthuppala98@gmail.com>
2022-03-29 12:22:43 +0530

    Initial Commit

---
+++ src/test/java/GitPractice/GitPractice/LoginTest.java
@@ -0,0 +1,30 @@
+package GitPractice.GitPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\java\\executables\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin(){
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("srikanthu598");
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		driver.quit();
+	}
+}
