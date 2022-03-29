2688aa4a33a0008c655620a4e99a5bcc9e03da8d
SrikanthUppala98 <srikanthuppala98@gmail.com>
2022-03-29 14:49:31 +0530

    branch checking

--- src/test/java/GitPractice/GitPractice/LoginTest.java
+++ src/test/java/GitPractice/GitPractice/LoginTest.java
@@ -20,6 +20,7 @@ public class LoginTest {
 	@Test
 	public void doLogin(){
 		//Test changes
+		//branch changes
 		driver.get("http://gmail.com");
 		driver.findElement(By.id("identifierId")).sendKeys("srikanthu598");
 	}
